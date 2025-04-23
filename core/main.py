39a8d31de0d525bd213481889721c40074e98596
Zcg321 <15gaudetz@gmail.com>
2025-04-23 13:04:46 -0400

    Update main.py

--- core/main.py
+++ core/main.py
@@ -10,8 +10,10 @@ sys.path.append(CORE_PATH)
 # === CORE MODULES ===
 from evolution_handler import handle_evolution
 from goal_pathfinder_handler import resolve_path
+from surgecell_handler import allocate_power
 
 if __name__ == "__main__":
     print("Jarvis Prime system initializing...")
     handle_evolution()
     resolve_path()
+    allocate_power("Core Systems", priority_level="high")
